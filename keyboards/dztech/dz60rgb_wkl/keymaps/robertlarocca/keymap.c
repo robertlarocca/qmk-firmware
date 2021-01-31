16cea1da433f9e9a7e0456f5c7aa194846ae4ed3
Robert <29333852+robertlarocca@users.noreply.github.com>
2021-01-31 13:20:54 -0500

    Improved shift + backspace = delete support for robertlarocca keymap  - shift + backspace = delete  - shift + shift + backspace = shift + delete

--- keyboards/dztech/dz60rgb_wkl/keymaps/robertlarocca/keymap.c
+++ keyboards/dztech/dz60rgb_wkl/keymaps/robertlarocca/keymap.c
@@ -1,47 +1,49 @@
 #include QMK_KEYBOARD_H
 
 enum custom_keycodes {
-	BSPC_DEL,	// double-tap backspace for delete
-	KC_BSDL,	// shift backspace for delete
+	SHIFT_BSPC_DEL,	// shift + backspace = delete
 };
+
+#define KC_BSDL SHIFT_BSPC_DEL
 
 enum custom_layers {
-	BASE = 0,	// default layer
+	BASE = 0,	// base layer (default)
 	FUNC = 1,	// function layer
 	LIGHT = 2,	// lighting layer
-/*
-	MACRO = 3,	// macro layer
-	MISC = 4,	// miscellaneous layer
-	NAVI = 5,	// navigation layer
-	SHIFT = 6,	// shift layer
-*/
 };
 
-qk_tap_dance_action_t tap_dance_actions[] = {
-	[BSPC_DEL] = ACTION_TAP_DANCE_DOUBLE(KC_BSPC, KC_DEL)
-};
+static uint16_t KC_SHIFT = 0;
+static bool shift_status = false;
+static bool bspc_del_mod = false;
 
-const uint8_t shift = MOD_BIT(KC_LSFT) | MOD_BIT(KC_RSFT);
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-	switch(keycode) {
-	case KC_BSDL:
+	switch (keycode) {
+	case KC_LSFT:
+		shift_status = record->event.pressed;
+		KC_SHIFT = keycode;
+		return true;
+		break;
+	case KC_RSFT:
+		shift_status = record->event.pressed;
+		KC_SHIFT = keycode;
+		return true;
+		break;
+	case SHIFT_BSPC_DEL:
 		if (record->event.pressed) {
-			if (keyboard_report->mods & shift) {
-				if (keyboard_report->mods & MOD_BIT(KC_LSFT)) {
-					unregister_code(KC_LSHIFT);
-				}
-				else {
-					unregister_code(KC_RSHIFT);
-				}
+			if (shift_status) {
+				unregister_code(KC_SHIFT);
 				register_code(KC_DEL);
-			}
-			else {
+				bspc_del_mod = true;
+			} else {
 				register_code(KC_BSPC);
 			}
-		}
-		else {
-			unregister_code(KC_DEL);
-			unregister_code(KC_BSPC);
+		} else {
+			if (bspc_del_mod) {
+				unregister_code(KC_DEL);
+				bspc_del_mod = false;
+			} else {
+				unregister_code(KC_BSPC);
+			}
 		}
 		return false;
 		break;
@@ -52,7 +54,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[BASE] = LAYOUT_60_tsangan_hhkb(
 		KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_GRV, KC_BSLS,
-		KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, TD(BSPC_DEL),
+		KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSDL,
 		KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
 		KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, TT(1),
 		KC_NO, KC_LGUI, KC_LCTL, KC_SPC, KC_RALT, KC_APP, KC_NO
@@ -65,7 +67,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, TT(2), KC_NO
 	),
 	[LIGHT] = LAYOUT_60_tsangan_hhkb(
-		TO(0), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		TO(1), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
 		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RGB_HUD, RGB_HUI, RGB_SAD, RGB_SAI, RGB_SPD, RGB_SPI, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
 		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, BL_TOGG, RGB_TOG, RGB_VAD, RGB_VAI, RGB_RMOD, RGB_MOD, KC_TRNS, KC_TRNS, KC_TRNS,
 		KC_TRNS, KC_TRNS, KC_TRNS, EEP_RST, DEBUG, RESET, NK_TOGG, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
